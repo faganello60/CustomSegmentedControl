e545868ed838587fe8c81e490d4c45aa0bb8cc98
Bruno Faganello <bruno.faganello.neto@gmail.com>
2018-07-08 23:26:36 -0300

    Initial Commit

---
+++ CustomSegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSegmentedControl
+//
+//  Created by Bruno Faganello Neto on 08/07/18.
+//  Copyright © 2018 Faganello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
